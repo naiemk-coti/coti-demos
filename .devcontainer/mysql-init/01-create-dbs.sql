e293c7a810f4f86d1f634f0ab5ad41a23b5b29ba
Naiem <naiem@coti.io>
2026-04-15 23:59:33 +0300

    Remove devcontainer'

--- .devcontainer/mysql-init/01-create-dbs.sql
+++
@@ -1,4 +0,0 @@
--- Create databases for treasury (main + db_sync + gcoti snapshot) when running in devcontainer.
-CREATE DATABASE IF NOT EXISTS treasury_dev;
-CREATE DATABASE IF NOT EXISTS db_app_dev;
-CREATE DATABASE IF NOT EXISTS treasury_gcoti_rewards;
